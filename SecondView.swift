241b9b7c54ae773f2493ef9b5310799fd71ea78b
David Nitzsche-Bell <david@zicount.com>
2022-03-15 17:29:33 +0100

    Add second page View

---
+++ SecondView.swift
@@ -0,0 +1,20 @@
+//
+//  SecondView.swift
+//  GitHubDemo
+//
+//  Created by Zicount on 15.03.22.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SecondView_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondView()
+    }
+}
