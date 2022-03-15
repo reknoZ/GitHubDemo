2a722afda4060e81d0e0234c3172d1f35fa13574
David Nitzsche-Bell <david@zicount.com>
2022-03-15 17:20:41 +0100

    Initial Commit

---
+++ GitHubDemo/GitHubDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubDemoApp.swift
+//  GitHubDemo
+//
+//  Created by Zicount on 15.03.22.
+//
+
+import SwiftUI
+
+@main
+struct GitHubDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
